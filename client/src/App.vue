dc18bae5743ddadbd8610210c5ee176f735800a1
Mathias LEROY <mathias.leroy@epsi.fr>
2023-02-01 15:27:18 +0100

    feat/ajout du router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <AnnonceItem />
+  <a href="#/">Liste des annonces</a> | <a href="#/plantepage{}">Plante</a> |
+  <a href="#/non-existent-path">Broken Link</a>
+  <component :is="currentView" />
 </template>
 
 <script>
 import "./assets/css/style.css";
-import AnnonceItem from "./components/AnnonceItem.vue";
+import PlantePage from "./views/PlantePage.vue";
+import ListeAnnonces from "./views/ListeAnnonces.vue";
 
 export default {
   name: "App",
   components: {
-    AnnonceItem,
+    PlantePage,
+    ListeAnnonces,
+  },
+};
+</script>
+
+<script>
+import PlantePage from "./views/PlantePage.vue";
+import ListeAnnonces from "./views/ListeAnnonces.vue";
+
+const routes = {
+  "/": ListeAnnonces,
+  "/about": About,
+};
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"] || NotFound;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
   },
 };
 </script>
---
+++ client/src/views/ListeAnnonces.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Liste des annonces</h1>
+    <AnnonceItem />
+  </div>
+</template>
+
+<script>
+import "./assets/css/style.css";
+import AnnonceItem from "../components/AnnonceItem.vue";
+
+export default {
+  name: "ListeAnnonces",
+  components: {
+    AnnonceItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--color-primary);
+}
+
+body {
+  margin: 0;
+}
+</style>
