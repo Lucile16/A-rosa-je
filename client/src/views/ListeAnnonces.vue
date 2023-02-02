35ee4efbe7c80eb6dd4abd1d353328fc913aea8c
Mathias LEROY <mathias.leroy@epsi.fr>
2023-02-02 12:29:15 +0100

    feat/ afficher un msg lorsqu'il y a aucune annonce

--- client/src/views/ListeAnnonces.vue
+++ client/src/views/ListeAnnonces.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>Liste des annonces</h1>
   <div class="container">
-    <AnnonceCardComponent
-      v-for="a in annonces"
-      :key="a.id"
-      :title="a.title"
-      :description="a.description"
-    />
+    <div v-if="!annonces.length">
+      <p>Il n'y a aucune annonce de disponible actuellement</p>
+    </div>
+    <div v-else>
+      <AnnonceCardComponent
+        v-for="a in annonces"
+        :key="a.id"
+        :title="a.title"
+        :description="a.description"
+      />
+    </div>
   </div>
 </template>
 
@@ -34,20 +39,20 @@ export default {
     };
   },
   created() {
-        this.fetchAnnonces();
+    this.fetchAnnonces();
+  },
+  methods: {
+    async fetchAnnonces() {
+      try {
+        const response = await axios.get("http://localhost:8080/annonces");
+        console.log(response.data._embedded.annonces);
+        this.annonces = response.data._embedded.annonces;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    methods: {
-      async fetchAnnonces() {
-        try {
-            const response = await axios.get('http://localhost:8080/annonces');
-            console.log(response.data._embedded.annonces);
-            this.annonces = response.data._embedded.annonces;
-        } catch (error) {
-            console.error(error);
-        }
-        }
-    }
-  };
+  },
+};
 </script>
 
 <style scoped>
