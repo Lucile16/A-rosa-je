5988fc881861cdf7fa7312f0f2582c8eef49f28d
PierreGoupil13 <p.goupil356@gmail.com>
2023-02-09 14:12:03 +0100

    Ajout de photo en BDD sous forme d'URL

--- client/src/components/PlanteComponent.vue
+++ client/src/components/PlanteComponent.vue
@@ -2,19 +2,17 @@
   <div class="container text-center">
     <h1>{{ plante.nom }}</h1>
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <img
-            src="https://media.istockphoto.com/photos/young-plant-growing-in-sunlight-picture-id658291850?b=1&k=20&m=658291850&s=612x612&w=0&h=a0XOE6YCZQeeoOesqPfZPVBVRzJLENkjfIoYCSi-a1g="
-            alt="Plante"
-          />
+      <div class="row align-items-center">
+        <div class="col gx-5">
+          <img v-bind:src="plante.photo" alt="Plante" />
         </div>
-        <div class="col">
+        <div class="col gx-5">
           <div>Famille de la plante : {{ plante.famille }}</div>
           <div>Dimension de la plante : {{ plante.dimension }}</div>
           <div>Exposition de la plante : {{ plante.exposition }}</div>
           <div>Arrosage de la plante : {{ plante.arrosage }}</div>
           <div>Description de la plante : {{ plante.description }}</div>
+          <div>Description de la plante : {{ plante.photo }}</div>
         </div>
       </div>
     </div>
